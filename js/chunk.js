1b520f8957a4318408ff43c9791b75c4bc7617be
DrakeOne <124852325+DrakeOne@users.noreply.github.com>
2025-08-08 10:16:13 -0400

    Agregar chunk.js para compatibilidad

---
+++ js/chunk.js
@@ -0,0 +1,9 @@
+/**
+ * VoxelCraft - Sistema de Chunks
+ * v0.1.0
+ * 
+ * Clase Chunk para compatibilidad
+ */
+
+// Re-exportar la clase Chunk desde world.js para compatibilidad
+// Este archivo existe para mantener la estructura esperada por index.html
